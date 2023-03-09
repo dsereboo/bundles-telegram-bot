125dc91a6af46083e3fe07ecae078822bbf5417a
Daniel <agyemang-sereboo@pces.mk>
2023-03-09 12:25:38 +0000

    Bot flow completed

--- commands/bot/add.ts
+++ commands/bot/add.ts
@@ -1,23 +1,14 @@
-import { AddItems } from "../../utils/Operations";
+import { BotSession } from "../../types/common";
+import { AddItems, parseNums } from "../../utils/Operations";
 import { StartContext } from "../global/start";
 
-export default function addCommand (ctx:StartContext){
-    let input = ctx.message.text;
-    
-    let inputArray = input.split(" ");
-  
-    if (inputArray.length > 1){
-    //   const params = inputArray.slice(1)
-      const nums= inputArray.map((str)=>{return parseInt(str)})
-  
-      if(nums.includes(NaN)){
-          return ctx.reply('Invalid input! 😢 Please try again')
-      }
-      else{
-          const sum = nums.reduce(AddItems,0)
-          return ctx.reply(`Result is ${sum}`)
-      }
-    }
-    return ctx.reply(`result`);
-}
+export default function addCommand(ctx: BotSession) {
+  let input = parseNums(ctx?.session?.firstNum, ctx?.session?.secondNum);
 
+  if (input.includes(NaN)) {
+    return ctx.reply("Invalid input! 😢 Please try again");
+  } else {
+    const sum = input.reduce(AddItems, 0);
+    return ctx.reply(`Result is ${sum}`);
+  }
+}
--- commands/utils/Unknown.ts
+++ commands/utils/Unknown.ts
@@ -5,21 +5,21 @@
 // })
 
 
-export default function unknown(ctx, next) {
-  const commandRegex =
-    /^(?!(.*\/add)|(.*\/subtract)|(.*\/multiply)|(.*\/divide)|(.*\/start)|(.*\/help)).*$/;
-  if (commandRegex.test(ctx.message.text)) {
-    return ctx.reply("Sorry, can't understand your command.");
-  } else {
-    return next();
-  }
-}
+// export default function unknown(ctx, next) {
+//   const commandRegex =
+//     /^(?!(.*\/add)|(.*\/subtract)|(.*\/multiply)|(.*\/divide)|(.*\/start)|(.*\/help)).*$/;
+//   if (commandRegex.test(ctx.message.text)) {
+//     return ctx.reply("Sorry, can't understand your command.");
+//   } else {
+//     return next();
+//   }
+// }
 
 
-ctx.session ??= {action:" "}
-ctx.session ??= {action:" "}
-ctx.session ??= {action:" "}
-ctx.session ??= {action:" "}
+// ctx.session ??= {action:" "}
+// ctx.session ??= {action:" "}
+// ctx.session ??= {action:" "}
+// ctx.session ??= {action:" "}
 
 // bot.action("Add", (ctx) => {
 //   ctx.answerCbQuery();
@@ -48,3 +48,150 @@ ctx.session ??= {action:" "}
 //     ctx.session.action="divide"
 //     ctx.reply("Enter the two numbers for division");
 // });
+
+//Unknown commands
+// bot.on(message("text"), (ctx, next) => {
+//   const commandRegex =
+//     /^(?!(.*\/add)|(.*\/subtract)|(.*\/multiply)|(.*\/divide)|(.*\/start)|(.*\/help)).*$/;
+//   if (commandRegex.test(ctx.message.text)) {
+//     return ctx.reply("Sorry, can't understand your command.");
+//   } else {
+//     return next();
+//   }
+// });
+
+
+
+//Listener
+// bot.on(message("text"), async(ctx)=>{
+//     switch (ctx?.session?.action.toLocaleLowerCase()){
+//         case "add":
+//             await addCommand(ctx)
+//             ctx.session = {action:" "}
+//             break;
+//         case "subtract":
+//             await subtractCommand(ctx)
+//             ctx.session = {action:" "}
+//             break;
+//         case "divide":
+//             await divideCommand(ctx)
+//             ctx.session = {action:" "}
+//             break;
+//         case "multiply":
+//             await multiplyCommand(ctx)
+//             ctx.session = {action:" "}
+//             break;
+//         default:
+//            await ctx.reply("❗Invalid action. Please try again")
+//     }
+//     //clear action
+
+//     //Another operation
+//     ctx.reply("What will be your next operation?", {
+//         reply_markup: {
+//           keyboard: [
+//               [
+//                   { text: "Add",   },
+//               ],
+//               [
+//                 { text: "Subtract", },
+//               ],
+//               [
+//                 { text: "Multiply", },
+//               ],
+//               [
+//                 { text: "Divide",},
+//               ]
+//           ],
+//           resize_keyboard:true,
+//           one_time_keyboard:true,
+//         },
+//       })
+//       //Another operation(after a number of operations)
+//     //Terminate session
+//     //Reshow operations.
+//     //auto trigger number keyboard when button is clicked???
+// })
+
+ // ctx.session.firstNum !== ""
+  //   ? 
+  //   (
+      
+  //     number !== NaN? " ": ""
+  //   )
+  //   : (ctx.session = { ...ctx.session, firstNum: ctx.message.text });
+
+  // if (ctx?.session?.secondNum !== "") {
+  //   switch (ctx?.session?.action) {
+  //     case "Add":
+  //       await addCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Subtract":
+  //       await subtractCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Divide":
+  //       await divideCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Multiply":
+  //       await multiplyCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     default:
+  //       await ctx.reply("❗Invalid action. Please try again");
+  //       nextOperationReply(ctx)
+  //   }
+  // } else {
+  //   ctx.reply("Enter second number\ne.g 24");
+  // }
+
+
+  /if parse fails throw reply
+  // if(Number.isNaN(number)){
+  //   await ctx.reply("❗Invalid input. Your input should be a number.\ne.g 23");
+  //   nextOperationReply(ctx)
+  // }
+  // else if(Number.isInteger(number) && ctx.session.firstNum == "" ){
+  //      ctx.session = { ...ctx.session, firstNum: ctx.message.text }
+  // }
+  // else if(Number.isInteger(number) && ctx.session.firstNum !== ""){
+  //   ctx.session = { ...ctx.session, secondNum: ctx.message.text }
+  // }
+  // else if(ctx.session.firstNum !== "" && ctx?.session?.secondNum !== ""){
+  //   console.log(ctx.session)
+  //      switch (ctx?.session?.action) {
+  //     case "Add":
+  //       await addCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Subtract":
+  //       await subtractCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Divide":
+  //       await divideCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     case "Multiply":
+  //       await multiplyCommand(ctx);
+  //       ctx.session = defaultSession;
+  //       nextOperationReply(ctx)
+  //       break;
+  //     default:
+  //       await ctx.reply("❗Invalid action. Please try again");
+  //       nextOperationReply(ctx)
+  //   }
+  // }
+  // else{
+  //   ctx.reply("Enter second number\ne.g 23")
+  // }
+  // })
